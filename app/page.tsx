ff90a706c70fc115c35d19fd3a996eebbd207037
Mark(o) Woloschuk <mark@shtooky.com>
2026-06-21 19:31:06 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,33 @@
 
 import HeroAnimation from "./components/HeroAnimation"
 import EverythingIsInteresting from "./components/EverythingIsInteresting"
+import { TYPE } from "./components/Tokens"
+
+const BODY_STYLE = {
+    fontFamily: '"Source Serif 4", serif',
+    fontSize: `${TYPE.BODY_WELCOME.sizePx}px`,
+    fontWeight: TYPE.BODY_WELCOME.weight,
+    letterSpacing: `${TYPE.BODY_WELCOME.tracking}em`,
+    lineHeight: TYPE.BODY_WELCOME.lineHeight,
+    color: "#ffffff",
+}
 
 export default function Page() {
   return (
     <div style={{ position: "relative", width: "100%" }}>
       <div style={{ height: "35vh" }} />
-      <div style={{
-        width: "76%",
-        marginLeft: "auto",
-        marginRight: "auto",
-      }}>
+      <div style={{ width: "76%", marginLeft: "auto", marginRight: "auto" }}>
         <HeroAnimation />
         <div style={{ height: "10vh" }} />
         <EverythingIsInteresting />
+        <div style={{ height: "3vh" }} />
+        <p style={BODY_STYLE}>
+          This simple truth is in the heart of everything I do. I like to ask questions and think before I get my hands dirty. I want to understand all the whos and whats before I get to the hows.
+        </p>
+        <div style={{ height: "2vh" }} />
+        <p style={BODY_STYLE}>
+          Doing that means speaking fluent executive, marketer and engineer – I aim to be the gear that connects them all together in turning out business goals.
+        </p>
       </div>
     </div>
   )
